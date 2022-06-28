bd9c3458aa26b8562c3e7e73211238f0cd585f1b
N1GH7C4P <31281638+N1GH7C4P@users.noreply.github.com>
2022-06-28 17:05:24 +0300

    removed .git from p5 backend

---
+++ p5/backend/frontend/src/services/blogs.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+let token = null
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
+const getOne = (id) => {
+  const request = axios.get(`${baseUrl}/${id}`)
+  return request.then(response => response.data)
+}
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = async newObject => {
+
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = await axios.post(baseUrl, newObject, config)
+
+  return response.data
+}
+
+const update = (id, object) => {
+  const request = axios.put(`${baseUrl}/${id}`, object)
+  return request.then(response => response.data)
+}
+
+const remove = (removeId) => {
+  const request = axios.delete(`${baseUrl}/${removeId}`)
+  return request.then(response => response.data)
+}
+
+export default {
+  getOne: getOne,
+  getAll: getAll,
+  create: create,
+  update: update,
+  remove: remove,
+  setToken: setToken
+}
